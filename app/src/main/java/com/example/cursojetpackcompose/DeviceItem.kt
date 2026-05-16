377ba6381a1ee827e7ada2747cfc51ac4c131fd1
Sky <xpolargeist007x@gmail.com>
2026-05-15 21:51:56 -0600

    Modificamos el DeviceItem para acercarnos más a lo que queremos

--- app/src/main/java/com/example/cursojetpackcompose/DeviceItem.kt
+++ app/src/main/java/com/example/cursojetpackcompose/DeviceItem.kt
@@ -1,17 +1,27 @@
 package com.example.cursojetpackcompose
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cursojetpackcompose.ui.theme.CursoJetpackComposeTheme
 
 @Composable
 fun DeviceView(device: Device) {
-    Column {
-        Text(text = device.name)
-        Text(text = device.data?.color ?: "-")
-        Text(text = device.data?.capacity ?: "-")
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(imageVector = Icons.Default.Phone, contentDescription = null)
+
+        Column {
+            Text(text = device.name)
+            Text(text = device.data?.color ?: "-")
+            Text(text = device.data?.capacity ?: "-")
+        }
     }
 }
 
